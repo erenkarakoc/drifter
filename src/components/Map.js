bdf9408565f86dcf05d4eb82f7641c6996b9591c
erenkarakoc <erenkarakoc@aol.com>
2022-03-29 02:20:14 +0300

    Capacitor Geolocation added

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,19 @@
 // Ionic & React
 import { Component } from "react"
+import { Geolocation } from "@capacitor/geolocation"
 
 import Text from "./Text"
 
 export default class Map extends Component {
+  componentDidMount() {
+    const printCurrentPosition = async () => {
+      const coordinates = await Geolocation.getCurrentPosition()
+      console.log(coordinates)
+    }
+
+    printCurrentPosition()
+  }
+
   render() {
     return (
       <div>
